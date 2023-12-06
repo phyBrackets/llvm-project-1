ca4bb9501db94811e538a45e60cfb3c53a241813
Stephan T. Lavavej <stl@nuwen.net>
2023-12-06 06:04:06 -0800

    [libc++] Update <source_location> and msvc_stdlib_force_include.h (#74266)
    This makes libc++'s <filesystem> tests compatible with MSVC's STL.
    
    In msvc_stdlib_force_include.h, we need to define 3 more macros:
    
    - _CRT_DECLARE_NONSTDC_NAMES activates the POSIX names of
      `getcwd` etc. As the comment explains, we need this because
      we test with Clang `-fno-ms-compatibility`, which defines 
      `__STDC__` to `1`, which causes the UCRT headers to disable 
      the POSIX names by default.
    - Then we need _CRT_NONSTDC_NO_WARNINGS to avoid emitting
      deprecation warnings about the POSIX names.
    - Finally, we need `NOMINMAX` to seal away the ancient evil.
    
    These macros are documented in https://learn.microsoft.com/en-us/cpp/c-runtime-library/compatibility?view=msvc-170.
    
    As a drive-by change, the patch adds a "simulated" macro for 
    __has_feature(hwaddress_sanitizer). It also clang-formats all
    of msvc_stdlib_force_include.h and removes guards for
    __has_builtin(__builtin_source_location) in <source_location>,
    since those are not needed anymore.

--- libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
+++ libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
@@ -4032,17 +4032,11 @@
 #   error "__cpp_lib_smart_ptr_owner_equality should not be defined before c++26"
 # endif
 
-# if __has_builtin(__builtin_source_location) && !(defined(TEST_APPLE_CLANG_VER) && TEST_APPLE_CLANG_VER <= 1403)
-#   ifndef __cpp_lib_source_location
-#     error "__cpp_lib_source_location should be defined in c++20"
-#   endif
-#   if __cpp_lib_source_location != 201907L
-#     error "__cpp_lib_source_location should have the value 201907L in c++20"
-#   endif
-# else
-#   ifdef __cpp_lib_source_location
-#     error "__cpp_lib_source_location should not be defined when the requirement '__has_builtin(__builtin_source_location) && !(defined(TEST_APPLE_CLANG_VER) && TEST_APPLE_CLANG_VER <= 1403)' is not met!"
-#   endif
+# ifndef __cpp_lib_source_location
+#   error "__cpp_lib_source_location should be defined in c++20"
+# endif
+# if __cpp_lib_source_location != 201907L
+#   error "__cpp_lib_source_location should have the value 201907L in c++20"
 # endif
 
 # ifndef __cpp_lib_span
@@ -5563,17 +5557,11 @@
 #   error "__cpp_lib_smart_ptr_owner_equality should not be defined before c++26"
 # endif
 
-# if __has_builtin(__builtin_source_location) && !(defined(TEST_APPLE_CLANG_VER) && TEST_APPLE_CLANG_VER <= 1403)
-#   ifndef __cpp_lib_source_location
-#     error "__cpp_lib_source_location should be defined in c++23"
-#   endif
-#   if __cpp_lib_source_location != 201907L
-#     error "__cpp_lib_source_location should have the value 201907L in c++23"
-#   endif
-# else
-#   ifdef __cpp_lib_source_location
-#     error "__cpp_lib_source_location should not be defined when the requirement '__has_builtin(__builtin_source_location) && !(defined(TEST_APPLE_CLANG_VER) && TEST_APPLE_CLANG_VER <= 1403)' is not met!"
-#   endif
+# ifndef __cpp_lib_source_location
+#   error "__cpp_lib_source_location should be defined in c++23"
+# endif
+# if __cpp_lib_source_location != 201907L
+#   error "__cpp_lib_source_location should have the value 201907L in c++23"
 # endif
 
 # ifndef __cpp_lib_span
@@ -7313,17 +7301,11 @@
 #   endif
 # endif
 
-# if __has_builtin(__builtin_source_location) && !(defined(TEST_APPLE_CLANG_VER) && TEST_APPLE_CLANG_VER <= 1403)
-#   ifndef __cpp_lib_source_location
-#     error "__cpp_lib_source_location should be defined in c++26"
-#   endif
-#   if __cpp_lib_source_location != 201907L
-#     error "__cpp_lib_source_location should have the value 201907L in c++26"
-#   endif
-# else
-#   ifdef __cpp_lib_source_location
-#     error "__cpp_lib_source_location should not be defined when the requirement '__has_builtin(__builtin_source_location) && !(defined(TEST_APPLE_CLANG_VER) && TEST_APPLE_CLANG_VER <= 1403)' is not met!"
-#   endif
+# ifndef __cpp_lib_source_location
+#   error "__cpp_lib_source_location should be defined in c++26"
+# endif
+# if __cpp_lib_source_location != 201907L
+#   error "__cpp_lib_source_location should have the value 201907L in c++26"
 # endif
 
 # ifndef __cpp_lib_span
